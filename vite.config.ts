63f2286de108ba0ac2d2d8a9b573ef780292969a
Alex <alex.devenrgy@gmail.com>
2026-03-11 23:51:24 +0300

    chore: add vite-tsconfig-paths and configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
+		tsconfigPaths(),
 		react({
 			babel: {
 				plugins: [['babel-plugin-react-compiler']]
